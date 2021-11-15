4efc4d66ecb3a3af168936eb2e06722b93e5a26d
zhanghaibo <zhanghaibo0@huawei.com>
2021-11-15 20:49:54 +0800

    napi adapt previewer
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id:  If0b3c209f889c456bde88cdbcc24511bf46d8d21

--- interfaces/kits/napi/native_api.h
+++ interfaces/kits/napi/native_api.h
@@ -21,37 +21,35 @@
 #include "js_native_api.h"
 #include "native_common.h"
 
-napi_status napi_adjust_external_memory(napi_env env, int64_t change_in_bytes, int64_t* adjusted_value);
-napi_status napi_is_callable(napi_env env, napi_value value, bool* result);
-napi_status napi_create_runtime(napi_env env, napi_env* result_env);
-napi_status napi_serialize(napi_env env, napi_value object, napi_value transfer_list, napi_value* result);
-napi_status napi_deserialize(napi_env env, napi_value recorder, napi_value* object);
-napi_status napi_delete_serialization_data(napi_env env, napi_value value);
-napi_status napi_get_exception_info_for_worker(napi_env env, napi_value obj);
+NAPI_EXTERN napi_status napi_is_callable(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_create_runtime(napi_env env, napi_env* result_env);
+NAPI_EXTERN napi_status napi_serialize(napi_env env, napi_value object, napi_value transfer_list, napi_value* result);
+NAPI_EXTERN napi_status napi_deserialize(napi_env env, napi_value recorder, napi_value* object);
+NAPI_EXTERN napi_status napi_delete_serialization_data(napi_env env, napi_value value);
+NAPI_EXTERN napi_status napi_get_exception_info_for_worker(napi_env env, napi_value obj);
 napi_status napi_get_jsEngine(napi_env env, void** pEngine);
-napi_status napi_run_buffer_script(napi_env env, std::vector<uint8_t>& buffer, napi_value* result);
-napi_status napi_set_promise_rejection_callback(napi_env env, napi_ref ref, napi_ref checkRef);
+NAPI_EXTERN napi_status napi_run_buffer_script(napi_env env, std::vector<uint8_t>& buffer, napi_value* result);
+NAPI_EXTERN napi_status napi_set_promise_rejection_callback(napi_env env, napi_ref ref, napi_ref checkRef);
 
-napi_status napi_is_arguments_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_async_function(napi_env env, napi_value value, bool* result);
-napi_status napi_is_boolean_object(napi_env env, napi_value value, bool* result);
-napi_status napi_set_promise_rejection_callback(napi_env env, napi_ref ref, napi_ref checkRef);
-napi_status napi_is_generator_function(napi_env env, napi_value value, bool* result);
-napi_status napi_is_date(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_arguments_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_async_function(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_boolean_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_generator_function(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_date(napi_env env, napi_value value, bool* result);
 
-napi_status napi_is_map_iterator(napi_env env, napi_value value, bool* result);
-napi_status napi_is_set_iterator(napi_env env, napi_value value, bool* result);
-napi_status napi_is_generator_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_module_namespace_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_proxy(napi_env env, napi_value value, bool* result);
-napi_status napi_is_reg_exp(napi_env env, napi_value value, bool* result);
-napi_status napi_is_number_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_map(napi_env env, napi_value value, bool* result);
-napi_status napi_is_set(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_map_iterator(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_set_iterator(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_generator_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_module_namespace_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_proxy(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_reg_exp(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_number_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_map(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_set(napi_env env, napi_value value, bool* result);
 
-napi_status napi_is_string_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_symbol_object(napi_env env, napi_value value, bool* result);
-napi_status napi_is_weak_map(napi_env env, napi_value value, bool* result);
-napi_status napi_is_weak_set(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_string_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_symbol_object(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_weak_map(napi_env env, napi_value value, bool* result);
+NAPI_EXTERN napi_status napi_is_weak_set(napi_env env, napi_value value, bool* result);
 
 #endif /* FOUNDATION_ACE_NAPI_INTERFACES_KITS_NAPI_NATIVE_API_H */
