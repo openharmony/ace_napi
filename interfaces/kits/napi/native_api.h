3c4785e9e57d189c764fb24ddec5e213b867ea19
zhanghaibo <zhanghaibo0@huawei.com>
2022-01-14 14:25:54 +0800

    change header file of napi
    Change-Id: I2eefcdbaaea79b8147526b0c63134bdc2891b578
    Signed-off-by: zhanghaibo <zhanghaibo0@huawei.com>

--- interfaces/kits/napi/native_api.h
+++ interfaces/kits/napi/native_api.h
@@ -16,41 +16,224 @@
 #ifndef FOUNDATION_ACE_NAPI_INTERFACES_KITS_NAPI_NATIVE_API_H
 #define FOUNDATION_ACE_NAPI_INTERFACES_KITS_NAPI_NATIVE_API_H
 
-#include <vector>
-#include <iostream>
-#include "js_native_api.h"
-#include "native_common.h"
+#ifndef NAPI_VERSION
+#define NAPI_VERSION 8
+#endif
 
-NAPI_EXTERN napi_status napi_adjust_external_memory(napi_env env, int64_t change_in_bytes, int64_t* adjusted_value);
-NAPI_EXTERN napi_status napi_is_callable(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_create_runtime(napi_env env, napi_env* result_env);
-NAPI_EXTERN napi_status napi_serialize(napi_env env, napi_value object, napi_value transfer_list, napi_value* result);
-NAPI_EXTERN napi_status napi_deserialize(napi_env env, napi_value recorder, napi_value* object);
-NAPI_EXTERN napi_status napi_delete_serialization_data(napi_env env, napi_value value);
-NAPI_EXTERN napi_status napi_get_exception_info_for_worker(napi_env env, napi_value obj);
-NAPI_EXTERN napi_status napi_get_jsEngine(napi_env env, void** pEngine);
-NAPI_EXTERN napi_status napi_run_buffer_script(napi_env env, std::vector<uint8_t>& buffer, napi_value* result);
-NAPI_EXTERN napi_status napi_set_promise_rejection_callback(napi_env env, napi_ref ref, napi_ref checkRef);
+#ifndef NAPI_EXPERIMENTAL
+#define NAPI_EXPERIMENTAL
+#endif
 
-NAPI_EXTERN napi_status napi_is_arguments_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_async_function(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_boolean_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_generator_function(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_date(napi_env env, napi_value value, bool* result);
+#include <node_api.h>
 
-NAPI_EXTERN napi_status napi_is_map_iterator(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_set_iterator(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_generator_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_module_namespace_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_proxy(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_reg_exp(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_number_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_map(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_set(napi_env env, napi_value value, bool* result);
+#ifdef NAPI_TEST
+#ifdef _WIN32
+#define NAPI_INNER_EXTERN __declspec(dllexport)
+#else
+#define NAPI_INNER_EXTERN __attribute__((visibility("default")))
+#endif
+#else
+#ifdef _WIN32
+#define NAPI_INNER_EXTERN __declspec(deprecated)
+#else
+#define NAPI_INNER_EXTERN __attribute__((__deprecated__))
+#endif
+#endif
 
-NAPI_EXTERN napi_status napi_is_string_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_symbol_object(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_weak_map(napi_env env, napi_value value, bool* result);
-NAPI_EXTERN napi_status napi_is_weak_set(napi_env env, napi_value value, bool* result);
+NAPI_INNER_EXTERN napi_status napi_set_instance_data(napi_env env,
+                                                     void* data,
+                                                     napi_finalize finalize_cb,
+                                                     void* finalize_hint);
+
+NAPI_INNER_EXTERN napi_status napi_get_instance_data(napi_env env,
+                                                     void** data);
+
+NAPI_INNER_EXTERN napi_status napi_fatal_exception(napi_env env, napi_value err);
+
+NAPI_INNER_EXTERN napi_status napi_add_env_cleanup_hook(napi_env env,
+                                                        void (*fun)(void* arg),
+                                                        void* arg);
+
+NAPI_INNER_EXTERN napi_status napi_remove_env_cleanup_hook(napi_env env,
+                                                           void (*fun)(void* arg),
+                                                           void* arg);
+
+NAPI_INNER_EXTERN napi_status napi_add_async_cleanup_hook(
+    napi_env env,
+    napi_async_cleanup_hook hook,
+    void* arg,
+    napi_async_cleanup_hook_handle* remove_handle);
+
+NAPI_INNER_EXTERN napi_status napi_remove_async_cleanup_hook(
+    napi_async_cleanup_hook_handle remove_handle);
+
+NAPI_INNER_EXTERN napi_status napi_create_buffer(napi_env env,
+                                                 size_t length,
+                                                 void** data,
+                                                 napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_buffer_copy(napi_env env,
+                                                      size_t length,
+                                                      const void* data,
+                                                      void** result_data,
+                                                      napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_date(napi_env env,
+                                               double time,
+                                               napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_external_buffer(napi_env env,
+                                                          size_t length,
+                                                          void* data,
+                                                          napi_finalize finalize_cb,
+                                                          void* finalize_hint,
+                                                          napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_bigint_int64(napi_env env,
+                                                       int64_t value,
+                                                       napi_value* result);
+
+NAPI_EXTERN napi_status napi_create_bigint_uint64(napi_env env,
+                                                        uint64_t value,
+                                                        napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_bigint_words(napi_env env,
+                                                       int sign_bit,
+                                                       size_t word_count,
+                                                       const uint64_t* words,
+                                                       napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_create_string_utf16(napi_env env,
+                                                       const char16_t* str,
+                                                       size_t length,
+                                                       napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_get_buffer_info(napi_env env,
+                                                   napi_value value,
+                                                   void** data,
+                                                   size_t* length);
+
+NAPI_INNER_EXTERN napi_status napi_get_date_value(napi_env env,
+                                                  napi_value value,
+                                                  double* result);
+
+NAPI_INNER_EXTERN napi_status napi_get_value_bigint_int64(napi_env env,
+                                                          napi_value value,
+                                                          int64_t* result,
+                                                          bool* lossless);
+
+NAPI_EXTERN napi_status napi_get_value_bigint_uint64(napi_env env,
+                                                           napi_value value,
+                                                           uint64_t* result,
+                                                           bool* lossless);
+
+NAPI_INNER_EXTERN napi_status napi_get_value_bigint_words(napi_env env,
+                                                          napi_value value,
+                                                          int* sign_bit,
+                                                          size_t* word_count,
+                                                          uint64_t* words);
+
+NAPI_INNER_EXTERN napi_status napi_get_value_string_utf16(napi_env env,
+                                                          napi_value value,
+                                                          char16_t* buf,
+                                                          size_t bufsize,
+                                                          size_t* result);
+
+NAPI_INNER_EXTERN napi_status napi_is_buffer(napi_env env,
+                                             napi_value value,
+                                             bool* result);
+
+NAPI_INNER_EXTERN napi_status napi_detach_arraybuffer(napi_env env,
+                                                      napi_value arraybuffer);
+
+NAPI_INNER_EXTERN napi_status napi_is_detached_arraybuffer(napi_env env,
+                                                           napi_value value,
+                                                           bool* result);
+
+NAPI_INNER_EXTERN napi_status napi_get_all_property_names(napi_env env,
+                                                          napi_value object,
+                                                          napi_key_collection_mode key_mode,
+                                                          napi_key_filter key_filter,
+                                                          napi_key_conversion key_conversion,
+                                                          napi_value* result);
+
+NAPI_EXTERN napi_status napi_object_freeze(napi_env env, napi_value object);
+
+NAPI_INNER_EXTERN napi_status napi_object_seal(napi_env env,
+                                               napi_value object);
+
+NAPI_INNER_EXTERN napi_status napi_type_tag_object(napi_env env,
+                                                   napi_value value,
+                                                   const napi_type_tag* type_tag);
+
+NAPI_INNER_EXTERN napi_status napi_check_object_type_tag(napi_env env,
+                                                         napi_value value,
+                                                         const napi_type_tag* type_tag,
+                                                         bool* result);
+
+NAPI_INNER_EXTERN napi_status napi_add_finalizer(napi_env env,
+                                                 napi_value js_object,
+                                                 void* native_object,
+                                                 napi_finalize finalize_cb,
+                                                 void* finalize_hint,
+                                                 napi_ref* result);
+
+NAPI_INNER_EXTERN napi_status napi_async_init(napi_env env,
+                                              napi_value async_resource,
+                                              napi_value async_resource_name,
+                                              napi_async_context* result);
+
+NAPI_INNER_EXTERN napi_status napi_async_destroy(napi_env env,
+                                                 napi_async_context async_context);
+
+NAPI_INNER_EXTERN napi_status napi_make_callback(napi_env env,
+                                                 napi_async_context async_context,
+                                                 napi_value recv,
+                                                 napi_value func,
+                                                 size_t argc,
+                                                 const napi_value* argv,
+                                                 napi_value* result);
+
+NAPI_INNER_EXTERN napi_status napi_open_callback_scope(napi_env env,
+                                                       napi_value resource_object,
+                                                       napi_async_context context,
+                                                       napi_callback_scope* result);
+
+NAPI_INNER_EXTERN napi_status napi_close_callback_scope(napi_env env,
+                                                        napi_callback_scope scope);
+
+NAPI_INNER_EXTERN napi_status napi_adjust_external_memory(napi_env env,
+                                                          int64_t change_in_bytes,
+                                                          int64_t* adjusted_value);
+
+NAPI_INNER_EXTERN napi_status napi_create_threadsafe_function(napi_env env,
+                                                              napi_value func,
+                                                              napi_value async_resource,
+                                                              napi_value async_resource_name,
+                                                              size_t max_queue_size,
+                                                              size_t initial_thread_count,
+                                                              void* thread_finalize_data,
+                                                              napi_finalize thread_finalize_cb,
+                                                              void* context,
+                                                              napi_threadsafe_function_call_js call_js_cb,
+                                                              napi_threadsafe_function* result);
+
+NAPI_INNER_EXTERN napi_status napi_get_threadsafe_function_context(napi_threadsafe_function func,
+                                                                   void** result);
+
+NAPI_INNER_EXTERN napi_status napi_call_threadsafe_function(napi_threadsafe_function func,
+                                                            void* data,
+                                                            napi_threadsafe_function_call_mode is_blocking);
+
+NAPI_INNER_EXTERN napi_status napi_acquire_threadsafe_function(napi_threadsafe_function func);
+
+NAPI_INNER_EXTERN napi_status napi_release_threadsafe_function(napi_threadsafe_function func,
+                                                               napi_threadsafe_function_release_mode mode);
+
+NAPI_INNER_EXTERN napi_status napi_unref_threadsafe_function(napi_env env, napi_threadsafe_function func);
+
+NAPI_INNER_EXTERN napi_status napi_ref_threadsafe_function(napi_env env, napi_threadsafe_function func);
+
+NAPI_INNER_EXTERN napi_status node_api_get_module_file_name(napi_env env, const char** result);
 
 #endif /* FOUNDATION_ACE_NAPI_INTERFACES_KITS_NAPI_NATIVE_API_H */
