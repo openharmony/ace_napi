e6ec7ab17cff828443a0496181b8adfdc1d2115a
changjiaxing <changjiaxing1@huawei.com>
2022-03-11 12:07:30 +0800

    support bigint napi for ArkNativeEngine
    Signed-off-by: changjiaxing <changjiaxing1@huawei.com>

--- native_engine/impl/ark/ark_native_engine.cpp
+++ native_engine/impl/ark/ark_native_engine.cpp
@@ -26,6 +26,7 @@
 
 #include "native_value/ark_native_array.h"
 #include "native_value/ark_native_array_buffer.h"
+#include "native_value/ark_native_big_int.h"
 #include "native_value/ark_native_boolean.h"
 #include "native_value/ark_native_data_view.h"
 #include "native_value/ark_native_external.h"
@@ -53,6 +54,7 @@ using panda::NativePointerRef;
 using panda::SymbolRef;
 using panda::IntegerRef;
 using panda::DateRef;
+using panda::BigIntRef;
 static constexpr auto PANDA_MAIN_FUNCTION = "_GLOBAL::func_main_0";
 
 ArkNativeEngine::ArkNativeEngine(EcmaVM* vm, void* jsEngine) : NativeEngine(jsEngine), vm_(vm), topScope_(vm)
@@ -257,12 +259,12 @@ NativeValue* ArkNativeEngine::CreateNumber(double value)
 
 NativeValue* ArkNativeEngine::CreateBigInt(int64_t value)
 {
-    return nullptr;
+    return new ArkNativeBigInt(this, value);
 }
 
 NativeValue* ArkNativeEngine::CreateBigInt(uint64_t value)
 {
-    return nullptr;
+    return new ArkNativeBigInt(this, value, true);
 }
 
 NativeValue* ArkNativeEngine::CreateString(const char* value, size_t length)
@@ -789,7 +791,16 @@ NativeValue* ArkNativeEngine::CreateDate(double value)
 
 NativeValue* ArkNativeEngine::CreateBigWords(int sign_bit, size_t word_count, const uint64_t* words)
 {
-    return nullptr;
+    constexpr int bigintMod = 2; // 2 : used for even number judgment
+    bool sign = false;
+    if ((sign_bit % bigintMod) == 1) {
+        sign = true;
+    }
+    uint32_t size = (uint32_t)word_count;
+
+    Local<JSValueRef> value = BigIntRef::CreateBigWords(vm_, sign, size, words);
+
+    return new ArkNativeBigInt(this, value);
 }
 
 bool ArkNativeEngine::TriggerFatalException(NativeValue* error)
